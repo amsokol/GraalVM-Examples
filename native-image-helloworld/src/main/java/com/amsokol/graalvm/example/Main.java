4b96bfb5a9a7b0c859f170d7cb71a7c8c4b27876
Aleksandr Sokolovskii <github@amsokol.com>
2018-06-08 19:14:12 +0300

    added HelloWorld example

---
+++ native-image-helloworld/src/main/java/com/amsokol/graalvm/example/Main.java
@@ -0,0 +1,13 @@
+package com.amsokol.graalvm.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
+        // Prints "Hello, World" to the terminal window.
+        logger.info("Hello World");
+    }
+}
